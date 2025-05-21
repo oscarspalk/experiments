b022caf2300d18037b5c2db5b4c648a919ff7497
Oscar Spalk <oscar.gaardsted.spalk@gmail.com>
2025-05-21 17:30:04 +0200

    now we can move on to actually creating something

--- src/app.cpp
+++ src/app.cpp
@@ -23,7 +23,7 @@ namespace experiments
 
     void App::setup()
     {
-        ifont *font = OpenFont("LiberationSans", kFontSize, 0);
+        ifont *font = OpenFont("Roboto", kFontSize, 0);
 
         ClearScreen();
 
@@ -33,7 +33,7 @@ namespace experiments
         DrawLine(0, ScreenHeight() - 25, ScreenWidth(), ScreenHeight() - 25, 0x00666666);
         FillArea(50, 250, ScreenWidth() - 50 * 2, ScreenHeight() - 250 * 2, 0x00E0E0E0);
         FillArea(100, 300, ScreenWidth() - 100 * 2, ScreenHeight() - 300 * 2, 0x00A0A0A0);
-        DrawTextRect(0, ScreenHeight() / 2 - kFontSize / 2, ScreenWidth(), kFontSize, "Hello, world!", ALIGN_CENTER);
+        DrawTextRect(0, ScreenHeight() / 2 - kFontSize / 2, ScreenWidth(), kFontSize, "This is roboto now", ALIGN_CENTER);
         FullUpdate();
         CloseFont(font);
     }
