23cc9e243a3c557221721aee75f6e78168b189e4
Oscar Spalk <oscar.gaardsted.spalk@gmail.com>
2025-05-21 15:55:46 +0200

    made a script to remove old builds

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ static int main_handler(int event_type, int param_one, int param_two)
         DrawLine(0, ScreenHeight() - 25, ScreenWidth(), ScreenHeight() - 25, 0x00666666);
         FillArea(50, 250, ScreenWidth() - 50*2, ScreenHeight() - 250*2, 0x00E0E0E0);
         FillArea(100, 300, ScreenWidth() - 100*2, ScreenHeight() - 300*2, 0x00A0A0A0);
-        DrawTextRect(0, ScreenHeight()/2 - kFontSize/2, ScreenWidth(), kFontSize, "Hello, world!", ALIGN_CENTER);
+        DrawTextRect(0, ScreenHeight()/2 - kFontSize/2, ScreenWidth(), kFontSize, "Hello, world wirelessly!", ALIGN_CENTER);
 
         // Copies the buffer to the real screen
         FullUpdate();
